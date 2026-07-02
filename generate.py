efbcde50502a6a10786d3c790bb3863fccdeef5e
AAyani69 <aayani69@gmail.com>
2026-07-02 07:46:17 +0000

    Add static page generation

---
+++ generate.py
@@ -0,0 +1,15 @@
+
+from jinja2 import Environment, FileSystemLoader
+from euskalmet import get_wind_table
+
+data = get_wind_table("C042", n=20)
+
+env = Environment(loader=FileSystemLoader("templates"))
+template = env.get_template("index.html")
+
+html = template.render(data=data)
+
+with open("index.html", "w", encoding="utf-8") as f:
+    f.write(html)
+
+print("index.html generado correctamente")
